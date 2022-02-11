45c529440df2a2b1f18900c4e5cbfd74718bbd36
alexrezaei <74854699+alexrezaei@users.noreply.github.com>
2022-02-11 20:36:55 +0100

    update to homepage

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -6,19 +6,26 @@ function Accordion() {
         <div className="accordion-item">
             <h2 className="accordion-header" id="panelsStayOpen-headingOne">
             <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                Accordion Item #1
+                First
             </button>
             </h2>
             <div id="panelsStayOpen-collapseOne" className="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
             <div className="accordion-body">
+                <p>
+                Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus. 
+                </p>
                 <img className="img" src="/images/carousel-1.jpg" alt="" />
+                <div className="icons">
+                    <h5>Share</h5>
+                    <img className="icon" src="/images/twitter.png" alt="" />
+                </div>
             </div>
             </div>
         </div>
         <div className="accordion-item">
             <h2 className="accordion-header" id="panelsStayOpen-headingTwo">
             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-                Accordion Item #2
+                Second
             </button>
             </h2>
             <div id="panelsStayOpen-collapseTwo" className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
@@ -30,7 +37,7 @@ function Accordion() {
         <div className="accordion-item">
             <h2 className="accordion-header" id="panelsStayOpen-headingThree">
             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-                Accordion Item #3
+                Third
             </button>
             </h2>
             <div id="panelsStayOpen-collapseThree" className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
